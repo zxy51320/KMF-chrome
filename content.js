48f61874427db442a57561f57cc49605fc829a50
zxy51320 <78584947+zxy51320@users.noreply.github.com>
2023-10-13 15:52:28 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+// content.js
+setTimeout(function() {
+    document.querySelector(".shield-box.js-shield-box").style = null;
+    document.querySelector('.blur').style.filter = 'blur(0px)';
+    // 在这里插入您想要执行的代码
+}, 1000);
