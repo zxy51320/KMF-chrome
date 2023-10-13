5fe4079c37f81295a46a7172ce0e4d5fc39f0815
Brian <zxy51320@gmail.com>
2023-10-13 16:19:28 -0400

    减少等待时间

--- content.js
+++ content.js
@@ -3,4 +3,4 @@ setTimeout(function() {
     document.querySelector(".shield-box.js-shield-box").style = null;
     document.querySelector('.blur').style.filter = 'blur(0px)';
     // 在这里插入您想要执行的代码
-}, 1000);
+}, 700);
